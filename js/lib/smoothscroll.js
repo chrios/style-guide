98b490bcf44323c46d99175d510c5bfc8116bbbf
chrios <alpha@omega.eidolon>
2017-04-05 20:57:14 +0930

    modal and no-js

--- js/lib/smoothscroll.js
+++ js/lib/smoothscroll.js
@@ -1,5 +1,15 @@
-$('a[href*=\\#]').on('click', function(event){     
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+$('a[href*=\\#]').on('click', function(event){ 	//This wraps all <a href=#anyfoo></a> elements in jQuery object
+												//and when user click, performs the function:
+
+    event.preventDefault();						//Prevents the browser from scrolling normally to the jumplink target
+
+
+    $('html,body').animate({					//Wraps html body elements in jQuery object and
+    											//animates the following function:
+
+
+    	scrollTop:$(this.hash).offset().top 	//  scrolls the the offset of element from top of view
+
+    }, 800);		//conttrols speed of animation currently 800
 
 });
