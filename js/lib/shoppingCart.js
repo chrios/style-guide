cf02c0fd3e6b0070721b22b33ea4cfb361db808a
chrios <alpha@omega.eidolon>
2017-04-06 20:35:55 +0930

    wodn

--- js/lib/shoppingCart.js
+++ js/lib/shoppingCart.js
@@ -1,7 +1,7 @@
 if (typeof(Storage) !== "undefined") {
 
 	/*from http://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage */
-	/*allows arrays to be stored in localstorage as JSON using setObj(key, obj) and getObj(key) */
+	/*allows arrays to be manipulated in localstorage as JSON using setObj(key, obj) and getObj(key) */
 
 	Storage.prototype.setObj = function(key, obj) {
 	return this.setItem(key, JSON.stringify(obj))
@@ -20,80 +20,84 @@ if (typeof(Storage) !== "undefined") {
 					document.getElementById("listen__form").id, 
 					document.getElementById("listen__table").id ];
 
+	/* MAKE VARIABLE FOR BASKET__SEND */
+
+	var basketSend = document.getElementById("basket__send");
+
 	/* EVENT HANDLERS */
 
 	/*  */
 	/* if item is already associated with a json key when button is click the key is set to null */
 	/* otherwise the elementId is stored in json array within local storage (todo: change to session storage) */
 
-	document.getElementById("listen__colors").addEventListener("click",  	function(){	
-	    																		/*console.log(idArray[0]);*/
-																				if (localStorage.getObj(0) === idArray[0]) {
-																					localStorage.removeItem(0);
-																				} else {
-																					localStorage.setObj(0, idArray[0]);
-																				}
-																			});
+	idArray[0].addEventListener("click",  	function(){	
+												/*console.log(idArray[0]);*/
+												if (localStorage.getObj(0) === idArray[0]) {
+													localStorage.removeItem(0);
+												} else {
+													localStorage.setObj(0, idArray[0]);
+												}
+											});
 
-	document.getElementById("listen__headings").addEventListener("click", 	function(){	
-	    																		/*console.log(idArray[1]);*/
-																				if (localStorage.getObj(1) === idArray[1]) {
-																					localStorage.removeItem(1);
-																				} else {
-																					localStorage.setObj(1, idArray[1]);
-																				}
-																			});
+	idArray[1].addEventListener("click", 	function(){	
+												/*console.log(idArray[1]);*/
+												if (localStorage.getObj(1) === idArray[1]) {
+													localStorage.removeItem(1);
+												} else {
+													localStorage.setObj(1, idArray[1]);
+												}
+											});
 
-	document.getElementById("listen__paragraph").addEventListener("click", 	function(){	
-	    																		/*console.log(idArray[2]);*/
-																				if (localStorage.getObj(2) === idArray[2]) {
-																					localStorage.removeItem(2);
-																				} else {
-																					localStorage.setObj(2, idArray[2]);
-																				}
-																			});
+	idArray[2].addEventListener("click", 	function(){	
+												/*console.log(idArray[2]);*/
+												if (localStorage.getObj(2) === idArray[2]) {
+													localStorage.removeItem(2);
+												} else {
+													localStorage.setObj(2, idArray[2]);
+												}
+											});
 
-	document.getElementById("listen__list").addEventListener("click", 		function(){	
-	    																		/*console.log(idArray[3]);*/
-																				if (localStorage.getObj(3) === idArray[3]) {
-																					localStorage.removeItem(3);
-																				} else {
-																					localStorage.setObj(3, idArray[3]);
-																				}
-																			});
+	idArray[3].addEventListener("click", 		function(){	
+													/*console.log(idArray[3]);*/
+													if (localStorage.getObj(3) === idArray[3]) {
+														localStorage.removeItem(3);
+													} else {
+														localStorage.setObj(3, idArray[3]);
+													}
+												});
 
-	document.getElementById("listen__form").addEventListener("click",  		function(){	
-	    																		/*console.log(idArray[4]);*/
-																				if (localStorage.getObj(4) === idArray[4]) {
-																					localStorage.removeItem(4);
-																				} else {
-																					localStorage.setObj(4, idArray[4]);
-																				}
-																			});
+	idArray[4].addEventListener("click",  		function(){	
+													/*console.log(idArray[4]);*/
+													if (localStorage.getObj(4) === idArray[4]) {
+														localStorage.removeItem(4);
+													} else {
+														localStorage.setObj(4, idArray[4]);
+													}
+												});
 
-	document.getElementById("listen__table").addEventListener("click",  	function(){	
-	    																		/*console.log(idArray[5]);*/
-																				if (localStorage.getObj(5) === idArray[5]) {
-																					localStorage.removeItem(5);
-																				} else {
-																					localStorage.setObj(5, idArray[5]);
-																				}
-																			});
+	idArray[5].addEventListener("click",  	function(){	
+												/*console.log(idArray[5]);*/
+												if (localStorage.getObj(5) === idArray[5]) {
+													localStorage.removeItem(5);
+												} else {
+													localStorage.setObj(5, idArray[5]);
+												}
+											});
 	
 
-	document.getElementById("basket__send").addEventListener("click", 		function(){
-																				  /*here insted of console.log you would send the html + css of the element to the customer email address using php perhaps*/
-																				var listOfSelectedObjects = "<br>";
-																				for (var i = 0; i < localStorage.length; i++) {
-																					/*console.log( localStorage.key(i) +" has value " + localStorage[localStorage.key(i)] );*/
-																					listOfSelectedObjects += localStorage[localStorage.key(i)] + "<br>";
-																				}
-																				if (listOfSelectedObjects !== "<br>") {
-																					document.getElementById("basket__container--update").innerHTML = listOfSelectedObjects;
-																				} else {
-																					document.getElementById("basket__container--update").innerHTML = "None Selected!!!";
-																				}
-																			});
+	basketSend.addEventListener("click", 		function(){
+													  /*here insted of console.log you would send the html + css of the element to the customer email address using php perhaps*/
+													var listOfSelectedObjects = "<br>";
+													for (var i = 0; i < localStorage.length; i++) {
+														/*console.log( localStorage.key(i) +" has value " + localStorage[localStorage.key(i)] );*/
+														listOfSelectedObjects += localStorage[localStorage.key(i)] + "<br>";
+													}
+													if (listOfSelectedObjects !== "<br>") {
+														document.getElementById("basket__container--update").innerHTML = listOfSelectedObjects;
+													} else {
+														document.getElementById("basket__container--update").innerHTML = "None Selected!!!";
+													}
+												});
 
 } else {
     alert("Sorry! no localStorage support!");
